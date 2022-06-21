0c9f3c1c59b31c2c33e46a9d914f5b073a62f465
Caspar van Venrooij <casparvv52@gmail.com>
2022-06-21 14:44:48 +0200

    Add properties to init

--- urdfenvs/point_robot_urdf/envs/vel.py
+++ urdfenvs/point_robot_urdf/envs/vel.py
@@ -2,8 +2,8 @@ from urdfenvs.point_robot_urdf.envs.point_robot_env import PointRobotEnv
 
 
 class PointRobotVelEnv(PointRobotEnv):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, render=False, dt=0.01):
+        super().__init__(render=render, dt=dt)
 
     def apply_action(self, action):
         self._robot.apply_velocity_action(action)
